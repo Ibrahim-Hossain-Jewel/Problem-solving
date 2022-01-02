661ac1c3f57ca977ebbe4d736c1bb1ba27de5765
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-01-02 11:04:02 +0600

    reverse integer number without Math.sign()

--- reversingInt.js
+++ reversingInt.js
@@ -1,5 +1,12 @@
 //Reversing an Int value.
+//about parseInt() It will remove the fraction number and zero.
+//Math.sign() return 1 and -1 if the Math.sign() accept negative number then the Math.sign() return -1 and then if you provide positive number then return 1.
+
 function reversingInt (num){
-    return parseInt(num.toString().split("").reverse().join(""));
+    const reversed = parseInt(num.toString().split("").reverse().join(""));
+    if(num<0){
+        return reversed * -1;
+    }
+    return reversed;
 }
-console.log(reversingInt(981))
+console.log(reversingInt(-500))
