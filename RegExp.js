6af31e21e9b6d74298f8072dcc3e991dd38b85b1
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-03-12 10:36:10 +0600

    to remote special character from a string must use (or gate) in your regexp

--- RegExp.js
+++ RegExp.js
@@ -13,4 +13,18 @@ console.log(replacedCode);
 const isRight = verify.test(color);
 console.log(isRight)
 
+//write a RegExp code for verify the bangladeshi phone number
+//+8801794593235
+console.log("code for number verification")
+const number = '+8801794593235';
+const numberVerifier = /((\+?88)?01[1-9]{1,12})/g;
+console.log(numberVerifier.test(number));
+//Write a program that verifiy string the string doesn't consider special character 
+//select korla true otherwise false;
+console.log('string filter regular expression');
 
+//how to remove special character in javascript;
+
+const pure = 'Bangladesh$$%/\\';
+const countryRegExp = /\$|\%|\\|\//gi;
+console.log(pure.replace(countryRegExp,''))
